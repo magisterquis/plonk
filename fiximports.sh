416377d19e16249a59f8ea0c887f5c59c4205fa1
Stuart <jsmcmurrayjr@gmail.com>
2024-01-09 20:58:04 +0100

    Uncommit fiximports.sh

--- fiximports.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/ksh
-
-for f in $@; do
-        #sed -Ei.bak '/	"github.com\/magisterquis\/plonk_rewrite/d' "$f"
-        sed -Ei.bak 's/"github.com\/magisterquis\/plonk_rewrite/"github.com\/magisterquis\/plonk/' "$f"
-        goimports -d "$f"
-        echo "$f"
-        diff "$f" "$f.bak"
-done
